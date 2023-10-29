5f696506be15cc1a0f27dd761cfcfa9a7d1bec8e
Nitin-Tundwal <nitin1238kumar@gmail.com>
2023-10-29 19:14:54 +0530

    netlify commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div className="text-[32px] text-green-500">Home</div>;
+  return <div className="text-[32px] text-green-500">Nitin Tundwal</div>;
 };
 
 export default Home;
